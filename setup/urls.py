eb42a5a3cada737bc6cedb3f21967e1dc82a76d1
kelson <kelsontrd@gmail.com>
2025-05-23 23:48:02 -0300

    update config.py

--- setup/urls.py
+++ setup/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.views.generic import RedirectView
 
@@ -17,3 +19,6 @@ urlpatterns = [
     # Health check para Render
     path("healthz/", health_check),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
