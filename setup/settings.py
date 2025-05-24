37a0ea1f9e55feef419d727d54d6408c7fd637f5
kelson <kelsontrd@gmail.com>
2025-05-24 09:53:45 -0300

    update config.py

--- setup/settings.py
+++ setup/settings.py
@@ -34,6 +34,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.staticfiles.middleware.StaticFilesMiddleware",
 ]
 
 ROOT_URLCONF = "setup.urls"
