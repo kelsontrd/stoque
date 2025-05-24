2db868bd070bee3a8c4079830d9cbbc0c778a878
kelson <kelsontrd@gmail.com>
2025-05-24 13:13:05 -0300

    update

--- setup/settings.py
+++ setup/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["stoque-civm.onrender.com"]
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=(Csv()))
 
 INSTALLED_APPS = [
     "django.contrib.admin",
